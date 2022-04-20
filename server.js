5025cd3bfa57f0c028903ac7da16ac0c20b8789e
SakDev <saketh.devareddy1@gmail.com>
2022-04-20 01:11:21 -0400

    commit dos

--- server.js
+++ server.js
@@ -4,15 +4,78 @@ const app = express()
 
 const args = require('minimist')(process.argv.slice(2))
 
-args["port"]
+args["port", "debug", "log", "help"]
+console.log(args)
 
 const port = args["port"] || process.env.PORT || 5000
+const debug = args.debug || false
+const log = args.log || true
+const help = args.help
 
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
+const logdb = require("./database.js");
+const fs = require('fs');
+const morgan = require('morgan');
+
+const help_msg = (`
+server.js [options]
+--por		Set the port number for the server to listen on. Must be an integer
+              between 1 and 65535.
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+              a JSON access log from the database and /app/error which throws 
+              an error with the message "Error test successful." Defaults to 
+  false.
+--log		If set to false, no log files are written. Defaults to true.
+  Logs are always written to database.
+--help	Return this message and exit.
+`);
+
+if (args.help || args.h) {
+    console.log(helpMSG)
+    process.exit(0)
+}
+
+if (log == true) {
+    const accesslog = fs.createWriteStream('access.log', { flags: 'a' })
+    app.use(morgan('combined', {stream: accesslog}))
+} else {
+    console.log("No log written.")
+}
+
+if (args.debug === true) {
+    app.get('/app/log/access', (req, res) => {
+        const stmt = db.prepare('SELECT * FROM accesslog').all();
+        res.status(200).json(stmt)
+        //res.writeHead(res.statusCode, {"Content-Type" : "text/json"});
+    })
+
+    app.get('/app/error', (req, res) => {
+        throw new Error('Error test successful.')
+    })
+}
+
+app.use((req, res, next) => {
+    let logData = {
+            remoteaddr: req.ip,
+            remoteuser: req.user,
+            time: Date.now(),
+            method: req.method,
+            url: req.url,
+            protocol: req.protocol,
+            httpversion: req.httpVersion,
+            status: res.statusCode,
+            referer: req.headers['referer'],
+            useragent: req.headers['user-agent']
+        }
+        console.log(logData)
+        const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+        const info = stmt.run(logData.remoteaddr, logData.remoteuser, logData.time, logData.method, logData.url, logData.protocol, logData.httpversion, logData.status, logData.referer, logData.useragent)
+        next()
+    })
 
 function coinFlip() {
     return (Math.random() < 0.5 ? 'tails' : 'heads');
